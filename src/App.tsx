d4c4c9fe6b55b8ab49fd32b862b15a984c2cc0a7
John Carlo Austria <johncarloaustria@proton.me>
2022-10-01 14:36:31 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main>
+      <p className="text-3xl font-bold underline">
+        This is the start of the project markdone
+      </p>
+    </main>
+  )
+}
+
+export default App
