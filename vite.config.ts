2957df3e004d7be7ecfc4e5427959d14b04f2950
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2022-11-12 14:33:43 +0800

    Use sveltekit

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client"/>
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { sveltekit } from '@sveltejs/kit/vite';
+import type { UserConfig } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts'
-  }
-})
+const config: UserConfig = {
+	plugins: [sveltekit()]
+};
+
+export default config;
